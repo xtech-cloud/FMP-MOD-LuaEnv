b1dbc7db3d897355ed3df60a00c4f40175aa898c
Niall Liu <easlee@outlook.com>
2023-02-28 02:59:09 +0800

    add Recitation

---
+++ lsa-template/Recitation_6_0_0/style.lua
@@ -0,0 +1,18 @@
+return {
+    -- 抬头字体颜色
+    header_font_color = "#181818FF",
+    -- 文本文字颜色
+    text_font_color = "#3A3A3AFF",
+
+    -- 遮罩图片的九宫格边距
+    frame_border_left = 250,
+    frame_border_right = 250,
+    frame_border_top = 850,
+    frame_border_bottom = 200,
+
+    -- 遮罩图片的九宫格边距
+    mask_border_left = 0,
+    mask_border_right = 0,
+    mask_border_top = 98,
+    mask_border_bottom = 98,
+}
